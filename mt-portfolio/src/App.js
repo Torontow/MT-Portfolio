81700e9f9bcc7d873e8018d8f4b00b6d73a6f1e4
Torontow <mtorontow0@gmail.com>
2021-03-06 16:13:24 -0500

    Added some new components to fix up the rendering of the UI.

--- mt-portfolio/src/App.js
+++ mt-portfolio/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import GreatestHits from "./pages/GreatestHits"
-import Contact from "./pages/Contact"
+import GreatestHits from "./pages/GreatestHits";
+import Contact from "./pages/Contact";
+import Container from "./components/Container";
+import Row from "./components/Row";
+import Col from "./components/Col";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 // import './App.css';
 
 function App() {
@@ -12,10 +15,18 @@ function App() {
     <Router>
       <div>
         <Header />
-        <Navbar />
-        {/* <Route exact path="/" component={Home} /> */}
-        <Route exact path="/greatesthits" component={GreatestHits} />
-        <Route exact path="/contact" component={Contact} />
+        <Container>
+          <Row>
+            <Col size="3">
+              <Navbar />
+              {/* <Route exact path="/" component={Home} /> */}
+            </Col>
+            <Col size="9">
+              <Route exact path="/greatesthits" component={GreatestHits} />
+              <Route exact path="/contact" component={Contact} />
+            </Col>
+          </Row>
+        </Container>
         <Footer />
       </div>
     </Router>
