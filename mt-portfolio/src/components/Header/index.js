fd26f511807817dc2ec1270f5a2419f50ac80cbb
Torontow <mtorontow0@gmail.com>
2021-03-06 23:33:02 -0500

    Tweaked the margins and column sizes.

--- mt-portfolio/src/components/Header/index.js
+++ mt-portfolio/src/components/Header/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import iris from "../../images/iris.jpg"
+import "./style.css"
 
 function Header() {
   return (
-    <div className="container-fluid m-4 shadow" id="header">
+    <div className="container-fluid mt-2" id="header">
       <div className="row">
-        <div className="col-11">
+        <div className="col-10">
           <h1>Michael Torontow</h1>
         </div>
-        <div className="col-1 m-auto justCenter">
-          <a href="http://www.michaeltorontow.com" target="_blank">
-            <img src={iris} class="img-fluid border-rounded border-" alt="Iris" id="iris"></img>
+        <div className="col-2 m-auto">
+          <a href="http://www.michaeltorontow.com" target="_blank" rel="noreferrer">
+            <img src={iris} class="img-fluid rounded-circle" alt="Iris" id="iris"></img>
           </a>
         </div>
       </div>
