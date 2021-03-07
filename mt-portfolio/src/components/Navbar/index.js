44eee0f37e990352f266bf4e3bc675b8ff4c51b2
Torontow <mtorontow0@gmail.com>
2021-03-06 23:30:30 -0500

    Updated Navbar look and function.

--- mt-portfolio/src/components/Navbar/index.js
+++ mt-portfolio/src/components/Navbar/index.js
@@ -1,28 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import "./style.css";
 
 function Navbar() {
   return (
-    <nav>
-      <ul className="nav justify-content-center">
-        {/* <li className="nav-item">
-          <Link className="nav-link active" to="/">
-            Home
-          </Link>
-        </li> */}
-        <li className="nav-item">
-          <Link className="nav-link" to="/greatesthits">
-            Greatest Hits
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="contact">
-            Contact
-          </Link>
-        </li>
-      </ul>
+    <nav className="nav flex-columnn justify-content-end">
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          to="/greatesthits"
+          style={{ color: "darkslategray", textShadow: "1px 1px black" }}
+        >
+          Greatest Hits
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          to="contact"
+          style={{ color: "darkslategray", textShadow: "1px 1px black" }}
+        >
+          Reach Out
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          to="/"
+          style={{ color: "darkslategray", textShadow: "1px 1px black" }}
+        >
+          Bring Me Home
+        </Link>
+      </li>
     </nav>
   );
 }
