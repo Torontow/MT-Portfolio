a9badcd35d366f799cdaf1db93d99e37b58cdf56
Torontow <mtorontow0@gmail.com>
2021-03-06 23:49:40 -0500

    Added resume.

--- mt-portfolio/src/pages/Contact/index.js
+++ mt-portfolio/src/pages/Contact/index.js
@@ -3,6 +3,8 @@ import Row from "../../components/Row";
 import Col from "../../components/Col";
 import gitHubLogo from "../../images/gitHubLogo.png";
 import linkedinLogo from "../../images/linkedinLogo.png";
+import resume from "./MT-Web-Resume.pdf";
+
 import "./style.css";
 
 function Contact() {
@@ -16,7 +18,11 @@ function Contact() {
             target="_blank"
             rel="noreferrer"
           >
-            <img src={gitHubLogo} className="img-fluid logo" alt="Git Hub Logo" />
+            <img
+              src={gitHubLogo}
+              className="img-fluid logo"
+              alt="Git Hub Logo"
+            />
           </a>
         </Col>
         <a
@@ -24,13 +30,31 @@ function Contact() {
           target="_blank"
           rel="noreferrer"
         >
-          <img src={linkedinLogo} className="img-fluid logo" alt="LinkedIn Logo" />
+          <img
+            src={linkedinLogo}
+            className="img-fluid logo"
+            alt="LinkedIn Logo"
+          />
         </a>
       </Row>
-        <h4>Phone: <span className="contact">647-929-2341</span></h4>
-        <h4>
-          Email: <a href="mailto:mtorontow0@gmail.com" className="contact">mtorontow0@gmail.com</a>
-        </h4>
+      <h4>
+        Phone: <span className="contact">647-929-2341</span>
+      </h4>
+      <h4>
+        Email:{" "}
+        <a href="mailto:mtorontow0@gmail.com" className="contact">
+          mtorontow0@gmail.com
+        </a>
+      </h4>
+
+      <div class="mt-4">
+        <h2>Spy the Resume...</h2>
+      </div>
+      <div>
+        <a href={resume} download="MT-Web-Resume.pdf">
+          <h4>Resume</h4>
+        </a>
+      </div>
     </div>
   );
 }
