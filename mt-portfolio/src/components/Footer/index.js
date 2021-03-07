f79f01ff4ee16e12d1e46f6c1114072caafd8d6e
Torontow <mtorontow0@gmail.com>
2021-03-06 23:32:00 -0500

    Updated look and content of footer.

--- mt-portfolio/src/components/Footer/index.js
+++ mt-portfolio/src/components/Footer/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import "./style.css"
 
 function Footer() {
     return (
-        <div>
-            foot
-        </div>
+        <div class="container-fluid footer">
+        <footer>
+            <h6>&copy; 2020 Michael Torontow</h6>
+        </footer>
+    </div>
     )
 }
 
